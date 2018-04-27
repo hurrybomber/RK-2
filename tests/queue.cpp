7149f7f1b7084f6afbdfe2736194af7ffaba498c
hurrybomber <37066949+hurrybomber@users.noreply.github.com>
2018-04-27 23:15:41 +0400

    Update queue.cpp

--- tests/queue.cpp
+++ tests/queue.cpp
@@ -6,62 +6,62 @@ using namespace std;
 
 TEST_CASE("queue ini with other queue data")
 {
-    Queue <int> q1;
-    Queue <int> q2 = {1, 2, 3, 4};
-    Queue <int> q3{q2};
-    REQUIRE( q2 == q3);
+    Queue <int> a1;
+    Queue <int> a2 = {1, 2, 3, 4};
+    Queue <int> a3{a2};
+    REQUIRE( a2 == a3);
 }
 
 TEST_CASE("swap")
 {
-    Queue <int> q1;
-    Queue <int> q2 = {1, 2, 3, 4};
-    Queue <int> q3{q2};
-    Queue <int> temp = q3;
-    q1.swap(q3);
-    REQUIRE( q1 == temp);
-    REQUIRE( q3.empty() == true);
-    REQUIRE( q1.size() == 4);
+    Queue <int> a1;
+    Queue <int> a2 = {1, 2, 3, 4};
+    Queue <int> a3{a2};
+    Queue <int> temp = a3;
+    a1.swap(a3);
+    REQUIRE( a1 == temp);
+    REQUIRE( a3.empty() == true);
+    REQUIRE( a1.size() == 4);
 }
 
 TEST_CASE("push")
 {
-    Queue <int> q1;
-    Queue <int> q2 = {1, 2, 3, 4};
-    Queue <int> q3{q2};
-    q3.push(5);
-    REQUIRE( q3.size() == 5);
+    Queue <int> a1;
+    Queue <int> a2 = {1, 2, 3, 4};
+    Queue <int> a3{a2};
+    a3.push(5);
+    REQUIRE( a3.size() == 5);
 }
 TEST_CASE("pop#1")
 {
-    Queue<int> q4;
-    //если очередь пустая
-    q4.pop();
-    REQUIRE( q4.size() == 0);
+    Queue<int> a4;
+    //if the queue is empty
+    a4.pop();
+    REQUIRE( a4.size() == 0);
 }
 TEST_CASE("pop#2")
 {
-    Queue<int> q4 = {1};
-    q4.pop();
-    REQUIRE( q4.size() == 0);
+    Queue<int> a4 = {1};
+    a4.pop();
+    REQUIRE( a4.size() == 0);
 }
 TEST_CASE("pop#3")
 {
-    Queue<int> q4 = {1, 2, 3, 4, 5};
-    q4.pop();
-    REQUIRE( q4.size() == 4);
+    Queue<int> a4 = {1, 2, 3, 4, 5};
+    a4.pop();
+    REQUIRE( a4.size() == 4);
 }
 
 TEST_CASE("eq")
 {
-    Queue <int>  q1;
-    Queue <int>  q2 = {1, 2, 3, 4};
-    Queue <int>  q3{q2};
-    q3.push(5);
+    Queue <int>  a1;
+    Queue <int>  a2 = {1, 2, 3, 4};
+    Queue <int>  a3{q2};
+    a3.push(5);
     ofstream output{ "queue.bin" };
-    output << q3;
-    Queue <int> q4;
+    output << a3;
+    Queue <int> a4;
     ifstream input{ "queue.bin" };
-    input >> q4;
-    REQUIRE(! q4.empty() );
+    input >> a4;
+    REQUIRE(! a4.empty() );
 }
